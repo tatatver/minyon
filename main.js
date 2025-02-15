cbeaa3016dc20359be99f2cd83f10f61aceafe50
tatatver <tata.tver2001@gmail.com>
2025-02-15 14:16:51 +0500

    add adaptation

--- main.js
+++ main.js
@@ -1,62 +1,92 @@
-let USERNAME = "";
+let USERNAME;
 let POINT = 1000;
 let game_id;
-
-// Элементы
+//Элементы
 let points = document.getElementsByClassName("point");
-const gameBtn = document.querySelector("#gameBtn");
+let gameBTN = document.querySelector("#gameBTN");
 
-// Слушатели событий
-document.querySelector("#loginWrapper form").addEventListener("submit", (event) => {
+//Слушатели событий
+let forma = document
+  .querySelector("#loginWrapper form")
+  .addEventListener("submit", (event) => {
     event.preventDefault();
     auth();
   });
-[...points].forEach((elem) => elem.addEventListener("click", addPoint));
-gameBtn.addEventListener("click", startorStopGame);
+[...points].forEach((elem) => {
+  elem.addEventListener("click", addPoint);
+});
 
-// Функция для регистрации и логина
+gameBTN.addEventListener("click", startOrStopGame);
+
+//Функция для Регистрации и Входа
 async function auth() {
   const loginWrapper = document.getElementById("loginWrapper");
   const login = document.getElementById("login").value;
-
   let response = await sendRequest("user", "GET", { username: login });
-
   if (response.error) {
     alert("Вы не зарегистрированы");
-    let registration = await sendRequest("user", "POST", { username: login });
-
+    let registration = await sendRequest("user", "POST", {
+      username: login,
+    });
     if (!registration.error) {
-      alert("Мы вас зарегистрировали");
-      USERNAME = login;
+      alert("Мы Вас зарегистрировали");
       loginWrapper.style.display = "none";
-      updateUserBalanse();
+      USERNAME = response.username;
+      updateUserBalance();
     }
   } else {
-    USERNAME = login;
+    USERNAME = response.username;
     loginWrapper.style.display = "none";
-    updateUserBalanse();
+    updateUserBalance();
   }
 }
 
-// Функция для выбора баллов
+//Функция выбора очков
 function addPoint(event) {
-  let target = Number(event.target.innerHTML);
-  POINT = +target; // Обновляем переменную POINT
-  console.log(POINT);
-
-  let activePoints = document.querySelector(".point.active");
-  if (activePoints) {
-    activePoints.classList.remove("active");
+  let target = event.target.innerHTML;
+  POINT = +target;
+  let activePoint = document.querySelector(".point.active");
+  if (activePoint) {
+    activePoint.classList.remove("active");
   }
   event.target.classList.add("active");
 }
 
-// Функция обновления баланса пользователя
-async function updateUserBalanse() {
-  if (!USERNAME) return;
+//Функция для кнопки начала игры
+function startOrStopGame() {
+  if (gameBTN.innerHTML === "ИГРАТЬ") {
+    gameBTN.innerHTML = "ЗАВЕРШИТЬ ИГРУ";
+    gameBTN.style.backgroundColor = "red";
+    startGame();
+  } else {
+    gameBTN.innerHTML = "ИГРАТЬ";
+    gameBTN.style.backgroundColor = "#66a663";
+    stopGame();
+  }
+}
 
-  let response = await sendRequest("user", "GET", { username: USERNAME });
+//Функция для старта игры
+async function startGame() {
+  const payload = {
+    username: USERNAME,
+    points: POINT,
+  };
+  let response = await sendRequest("new_game", "POST", payload);
+  if (response.error) {
+    gameBTN.innerHTML = "ИГРАТЬ";
+    gameBTN.style.backgroundColor = "#66a663";
+  } else {
+    updateUserBalance();
+    game_id = response.game_id;
+    activateArea();
+  }
+}
 
+//Функция вывода имени пользоваеля и его баллов
+async function updateUserBalance() {
+  let response = await sendRequest("user", "GET", {
+    username: USERNAME,
+  });
   if (response.error) {
     alert(response.message);
   } else {
@@ -65,48 +95,7 @@ async function updateUserBalanse() {
   }
 }
 
-// Функция для кнопки начало игры
-function startorStopGame() {
-  if (gameBtn.innerHTML === "ИГРАТЬ") {
-    gameBtn.innerHTML = "ЗАВЕРШИТЬ ИГРУ";
-    gameBtn.style.backgroundColor = "red";
-    startGame();
-  } else {
-    gameBtn.innerHTML = "ИГРАТЬ";
-    gameBtn.style.backgroundColor = "#66a663";
-    stopGame();
-  }
-}
-
-// Функция для завершения игры
-async function stopGame() {
-  const response = await sendRequest("stop_game", "POST", {
-    username: USERNAME,
-    game_id,
-  });
-  console.log(response);
-  updateUserBalanse();
-  resetField();
-}
-
-// Функция для старта игры
-async function startGame() {
-  const payload = {
-    username: USERNAME,
-    points: POINT,
-  };
-  let response = await sendRequest("new_game", "POST", payload);
-  if (response.error) {
-    gameBtn.innerHTML = "ИГРАТЬ";
-    gameBtn.style.backgroundColor = "#66a663";
-  } else {
-    updateUserBalanse();
-    game_id = response.game_id;
-    activateArea();
-  }
-}
-
-// заполнение поля серым цветом
+//Функция для ативации поля (квадратики светлеют)
 function activateArea() {
   let field = document.getElementsByClassName("field");
 
@@ -119,83 +108,90 @@ function activateArea() {
     field[i].addEventListener("click", makeStep);
     setTimeout(() => {
       field[i].classList.add("active");
-    }, i * 30);
+    }, i * 35);
   }
 }
 
+//Функция для выбора ячейки
 async function makeStep(event) {
   const target = event.target;
   const row = +target.getAttribute("data-row");
   const column = +target.getAttribute("data-column");
-  console.log(row, column);
+
   try {
     const response = await sendRequest("game_step", "POST", {
       game_id,
       row: row,
       column: column,
     });
-    updateArea(response.table);
+updateArea(response.table)
     if (response.error) {
       alert(response.message);
     } else {
-      if (response.status === "ok") {
+      if (response.status === "Ok") {
       } else if (response.status === "Failed") {
         alert("Вы проиграли");
-        updateUserBalanse();
-        gameBtn.innerHTML = "ИГРАТЬ";
-        gameBtn.style.backgroundColor = "#66a663";
+        updateUserBalance();
+        gameBTN.innerHTML = "ИГРАТЬ";
+        gameBTN.style.backgroundColor = "#66a663";
         setTimeout(() => {
           resetField();
-        }, 2000);
+        }, 5000);
       } else if (response.status === "Won") {
-        alert("вы победитель");
-        updateUserBalanse();
-        gameBtn.innerHTML = "ИГРАТЬ";
-        gameBtn.style.backgroundColor = "#66a663";
+        alert("Вы победитель!");
+        updateUserBalance();
+        gameBTN.innerHTML = "ИГРАТЬ";
+        gameBTN.style.backgroundColor = "#66a663";
         setTimeout(() => {
           resetField();
-        }, 2000);
+        }, 5000);
       }
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
 
-function updateArea(table) {
-  let fields = document.querySelectorAll(".field");
-  let a = 0;
-  for (let i = 0; i < table.length; i++) {
-    let row = table[i];
-    for (let j = 0; j < row.length; j++) {
-      let cell = row[j];
-      let value = fields[a];
-      if (cell === "") {
-      } else if (cell === 0) {
-        value.classList.remove("active");
-      } else if (cell === "BOMB") {
-        value.classList.remove("active");
-        value.classList.add("bomb");
-      } else if (cell > 0) {
-        value.classList.remove("active");
-        value.innerHTML = cell;
-      }
-      a++;
+//Функция заполнения поля данным из БД
+function updateArea(table){
+let field = document.querySelectorAll(".field");
+let a = 0;
+for (let i= 0 ; i<table.length; i++){
+  let row = table[i];
+  for(let j=0; j< row.length; j++){
+    let cell = row[j];
+    let value = field[a];
+    if (cell ===""){
+      
+    }else if (cell === 0){
+value.classList.remove("active")
+    }else if(cell === "BOMB"){
+      value.classList.remove("active");
+      value.classList.add("bomb");
+    }else if(cell > 0){
+      value.classList.remove("active");
+      value.innerHTML = cell;
     }
+    a++;
   }
 }
-
-// Флажок
-function setFlag(event) {
-  event.preventDefault();
-  let target = event.target;
-  target.classList.toggle("flag");
 }
 
-// 80 клеток через жс
+//Функция для завершения игры
+async function stopGame() {
+  const response = await sendRequest("stop_game", "POST", {
+    username: USERNAME,
+    game_id,
+  });
+  console.log(response);
+  updateUserBalance();
+  resetField();
+}
+
+//Функция создания поля
 function resetField() {
   const gameField = document.querySelector(".gameField");
-  gameField.innerHTML = "";
+  gameField.innerHTML="";
   for (let i = 0; i < 80; i++) {
     const field = document.createElement("div");
     field.classList.add("field");
@@ -204,11 +200,17 @@ function resetField() {
 }
 resetField();
 
-// Функция для отправки запросов на сервер
+//Функция для деакцивации клика правой кнопкой мыши по полю и постановки флага
+function setFlag(event) {
+  event.preventDefault();
+  let target = event.target;
+  target.classList.toggle("flag");
+}
+
 async function sendRequest(url, method, data) {
   url = `https://tg-api.tehnikum.school/tehnikum_course/minesweeper/${url}`;
 
-  if (method === "POST") {
+  if (method == "POST") {
     let response = await fetch(url, {
       method: "POST",
       headers: {
@@ -218,8 +220,9 @@ async function sendRequest(url, method, data) {
       body: JSON.stringify(data),
     });
 
-    return await response.json();
-  } else if (method === "GET") {
+    response = await response.json();
+    return response;
+  } else if (method == "GET") {
     url = url + "?" + new URLSearchParams(data);
     let response = await fetch(url, {
       method: "GET",
@@ -228,7 +231,7 @@ async function sendRequest(url, method, data) {
         "Content-Type": "application/json",
       },
     });
-
-    return await response.json();
+    response = await response.json();
+    return response;
   }
 }
